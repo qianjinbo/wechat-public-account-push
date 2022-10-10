4897bfc3a1598026c320850a0984dc21aaacf08f
qianjinbo <115458545+qianjinbo@users.noreply.github.com>
2022-10-11 01:32:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqwkF5_0KvMHWScb-BvF7U6znRbE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '9-YgsVAaFeHY_sgl_K3CR-K7aZKwWA26-XBgDy5SeCQ',
+      useTemplateId: 'aeiNXg7MjshpXbN40ENYzoCEUk_W2cGHcnkP-__KP8g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-01',
       festivals: [
