3b60e0911becba2daa25ede0d4605e61a73a6b1b
qianjinbo <115458545+qianjinbo@users.noreply.github.com>
2022-10-11 01:44:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,8 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小一',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqwkF5_0KvMHWScb-BvF7U6znRbE'，
-      'oqwkF52ps0-U-u1EzH0LFXtAYbQY',
+      id: 'oqwkF52ps0-U-u1EzH0LFXtAYbQY'，
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'aeiNXg7MjshpXbN40ENYzoCEUk_W2cGHcnkP-__KP8g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -50,13 +49,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'aeiNXg7MjshpXbN40ENYzoCEUk_W2cGHcnkP-__KP8g',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oqwkF5_0KvMHWScb-BvF7U6znRbE',
     }
   ],
 
