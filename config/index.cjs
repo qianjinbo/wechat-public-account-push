54996e9c165b561475a9cb92231a15864f522d5b
qianjinbo <115458545+qianjinbo@users.noreply.github.com>
2022-10-11 01:18:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,38 +13,36 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '89aa37b5d9d008f2852f4f700c68b5b8',
 
-  PROVINCE: '安徽',
-  CITY: '合肥',
+  PROVINCE: '四川',
+  CITY: '成都',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小一',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqwkF5_0KvMHWScb-BvF7U6znRbE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'zxhcNRQtyld0CYrvmKgxk1p5D8Hy1OEzITe00WSnFv4',
+      useTemplateId: '9-YgsVAaFeHY_sgl_K3CR-K7aZKwWA26-XBgDy5SeCQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-01',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*日子', name: '与波波见面', year: '2023', date: '01-01',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小一宝', year: '2005', date: '01-01',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2021', date: '06-09',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2021-06-18' },
       ],
     },
   ],
